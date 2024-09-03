ec4a727b0fcd6faaab7a8e520b3244940bace85a
ehiosu <53658556+ehiosu@users.noreply.github.com>
2024-09-03 17:10:11 +0100

    base backend models  + project init

---
+++ school-sync/app/ConvexClient.tsx
@@ -0,0 +1,14 @@
+"use Client";
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import React from 'react'
+type Props = {
+    children:React.ReactNode
+}
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+export const ConvexClientProvider = (props: Props) => {
+  return (
+    <ConvexProvider client={convex}>
+        {props.children}
+    </ConvexProvider>
+  )
+}
---
+++ school-sync/convex/schema.ts
@@ -0,0 +1,62 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    school:defineTable({
+        name:v.string(),
+        subscription:v.id("subscription"),
+        adminId:v.id("admin"),
+        teacherIds:v.array(v.id("teacher")),
+        parentIds:v.array(v.id("parent")),
+        modules:v.array(v.object({
+            name:v.string(),
+            isEnabled:v.boolean()
+        }))
+    }),
+    subscription:defineTable({
+        name:v.string(),
+        price:v.number(),
+        enrolledIds:v.array(v.id("school"))
+    }),
+    user:defineTable({
+            username:v.string(),
+            email:v.optional(v.string()),
+            passwordHash:v.string(),
+            role:v.union(v.literal("TEACHER"),v.literal("STUDENT"),v.literal("PARENT"),v.literal("ADMIN")),
+            associatedId:v.string(),
+    }),
+    subject:defineTable({
+        name:v.string(),
+        units:v.number()
+    }),
+    teacher:defineTable({
+        firstName:v.string(),
+        lastName:v.string(),
+        subjects:v.array(v.id("subject")),
+        userId:v.id("user"),
+    }),
+    student:defineTable({
+        firstName:v.string(),
+        lastName:v.string(),
+        dateOfBirth:v.string(),
+        gradeLevel:v.string(),
+        userId:v.id("user"),
+        parentId:v.id("parent")
+    }),
+    parent:defineTable({
+        childrenIds:v.array(v.id("student")),
+        userId:v.id("user"),
+        schoolId:v.id("school")
+    }),
+    admin:defineTable({
+        firstName:v.string(),
+        lastName:v.string(),
+        userId:v.id("user"),
+    }),
+    superAdmin:defineTable({
+        username:v.string(),
+        email:v.optional(v.string()),
+        passwordHash:v.string(),
+      
+})
+})
